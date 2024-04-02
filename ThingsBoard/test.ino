7c1d44c595a606ffc02ea3d00a5584e79733070e
JhonMales <jhonmales777@gmail.com>
2024-04-02 09:21:46 -0500

    Nueva funcion
    Switch

--- ThingsBoard/test.ino
+++ ThingsBoard/test.ino
@@ -2,11 +2,11 @@
 #include <Arduino_MQTT_Client.h>
 #include <ThingsBoard.h>
 
-#define WIFI_AP ""
+#define WIFI_AP "Wokwi-GUEST"
 #define WIFI_PSWD ""
 
 #define TB_SERVER "demo.thingsboard.io"
-#define TOKEN ""
+#define TOKEN "zuul0uh30jvue3xap4pu"
 
 #define led_pin 13
 #define button_pin 14
@@ -18,92 +18,88 @@ int led_state = 0;
 
 RPC_Response setLedSwitchState(RPC_Data &data)
 {
-    Serial.println("Cambio de led recibido");
-    led_state = data;
-    Serial.println("Nuevo estado: ");
-    Serial.print(led_state);
-    if (led_state)
-    {
-      digitalWrite(led_pin, HIGH);
-    }
-    else
-    {
-      digitalWrite(led_pin, LOW);
-    }
-    return RPC_Response("setLedSwitchValue", led_state);
+  Serial.println("Cambio de led recibido");
+  led_state = data;
+  Serial.println("Nuevo estado: ");
+  Serial.print(led_state);
+  if (led_state)
+  {
+    digitalWrite(led_pin, HIGH);
+  }
+  else
+  {
+    digitalWrite(led_pin, LOW);
+  }
+  return RPC_Response("setLedSwitchValue", led_state);
 }
 bool subscribed = false;
 
-RPC_Response getButtonStatus()
-{
-  if(digitalRead(button_pin)==HIGH){
-    return RPC_Response("getButtonState", true);
-  }
-  else{
-    return RPC_Response("getButtonState", false);
-  }
-}
-
 const std::array<RPC_Callback, 1U> cb = {
-    RPC_Callback{"getButtonState",    getButtonStatus },
-    RPC_Callback("setLedSwitchValue", setLedSwitchState)};
+  RPC_Callback("setLedSwitchValue", std::bind(&setLedSwitchState, std::placeholders::_1))
+};
+
 
 void setup()
 {
-    Serial.begin(115200);
-    pinMode(26, OUTPUT);
-    digitalWrite(25, HIGH);
-    pinMode(button_pin, INPUT);
-    pinMode(led_pin, OUTPUT);
-    WiFi.begin(WIFI_AP, WIFI_PSWD, 6);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        Serial.print(".");
-        digitalWrite(led_pin, HIGH);
-        delay(250);
-        digitalWrite(led_pin, LOW);
-        delay(250);
-    }
-    Serial.println("Wifi connected!");
+  Serial.begin(9600);
+  pinMode(button_pin, INPUT_PULLUP);
+  pinMode(led_pin, OUTPUT);
+  WiFi.begin(WIFI_AP, WIFI_PSWD, 6);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    digitalWrite(led_pin, HIGH);
+    delay(250);
+    digitalWrite(led_pin, LOW);
+    delay(250);
+  }
+  Serial.println("Wifi connected!");
 }
 
 void loop()
 {
-    //Ensure Wifi connection
-    if (WiFi.status() != WL_CONNECTED)
+  //Ensure Wifi connection
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    while (WiFi.status() != WL_CONNECTED)
     {
-      while (WiFi.status() != WL_CONNECTED)
-      {
-          Serial.print(".");
-          digitalWrite(led_pin, HIGH);
-          delay(250);
-          digitalWrite(led_pin, LOW);
-          delay(250);
-      }
-      Serial.println("Wifi connected!");
+      Serial.print(".");
+      digitalWrite(led_pin, HIGH);
+      delay(250);
+      digitalWrite(led_pin, LOW);
+      delay(250);
     }
-    
-    //Connects to TB server
-    while (!tb.connected())
+    Serial.println("Wifi connected!");
+  }
+
+  //Connects to TB server
+  while (!tb.connected())
+  {
+    if (tb.connect(TB_SERVER, TOKEN))
     {
-        if (tb.connect(TB_SERVER, TOKEN))
-        {
-            Serial.println("Connected to TB server");
-        }
+      Serial.println("Connected to TB server");
     }
+  }
 
-    //Subscribe to RPC
-    while (!subscribed){
-      if (!tb.RPC_Subscribe(cb.begin(), cb.cend()))
-      {
-          Serial.println("Failed to subscribe RPC");
-          return;
-      }
-      else{
-        Serial.println("Subscribed to RPC");
-        subscribed = true;
-      }
+  //Subscribe to RPC
+  while (!subscribed) {
+    if (!tb.RPC_Subscribe(cb.begin(), cb.cend()))
+    {
+      Serial.println("Failed to subscribe RPC");
+      return;
     }
-
-    tb.loop();
+    else {
+      Serial.println("Subscribed to RPC");
+      subscribed = true;
+    }
+  }
+  if (tb.connected() && subscribed) {
+    if (digitalRead(button_pin) == HIGH) {
+      tb.sendTelemetryData("buttonState", true);
+    }
+    else {
+      tb.sendTelemetryData("buttonState", false);
+    }
+  }
+  tb.loop();
 }
